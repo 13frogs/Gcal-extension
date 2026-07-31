eeb41b4d3d8f066be52e520c20ed6cf8874ed361
13frogs <133548115+13frogs@users.noreply.github.com>
2026-07-31 14:57:46 -0400

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,35 @@
+document.getElementById("load").onclick = async () => {
+
+    const token = await getToken();
+
+    const data = await getCalendarEvents(token);
+
+    let totals = {};
+
+    data.items.forEach(event => {
+
+        if (!event.start.dateTime || !event.end.dateTime)
+            return;
+
+        let hours =
+            (new Date(event.end.dateTime) -
+             new Date(event.start.dateTime))
+             / 3600000;
+
+        let title = event.summary || "Other";
+
+        if (!(title in totals))
+            totals[title] = 0;
+
+        totals[title] += hours;
+    });
+
+    let html = "";
+
+    for (let cls in totals) {
+
+        html += `<p>${cls}: ${totals[cls].toFixed(2)} hrs</p>`;
+    }
+
+    document.getElementById("results").innerHTML = html;
+};
